8424fd3cf9319f987d7a3e90d07bb1936abdfa4f
Dedicio Coelho <dedicio@gmail.com>
2020-04-18 17:07:39 -0300

    Criação do card com Vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,56 @@
 <template>
   <div class="custom-card">
-    Teste
+    <div class="card has-background-dark">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              <img :src="imageURL" class="is-rounded" :alt="name" />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-5 is-marginless">{{ name }}</p>
+            <p class="subtitle is-6 is-marginless has-text-grey-light">{{ sub }}</p>
+            <p class="subtitle is-7">
+              <a href class="has-text-primary is-italic">{{ email }}</a>
+            </p>
+          </div>
+        </div>
+        <div class="content">
+          <p>{{ desc }}</p>
+          <div class="tags">
+            <a href="#" v-for="tag in tags" :key="tag" class="tag is-black">{{ tag }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Card"
+  name: "Card",
+  data() {
+    return {
+      imageURL: "http://placekitten.com/64/64",
+      name: "Dedicio Coelho",
+      sub: "35 anos | Joinvillie - SC",
+      email: "dedicio@gmail.com",
+      desc:
+        "Desenvolvedor Front End, formado em Produção Audiovisual pela UNIP.",
+      tags: [
+        "Javascript",
+        "HTML",
+        "CSS",
+        "Typescript",
+        "WordPress",
+        "PostgreSQL",
+        "MySQL",
+        "AWS",
+        "Bootstrap"
+      ]
+    };
+  }
 };
 </script>
 
@@ -15,5 +59,12 @@ export default {
   height: 100%;
   width: 100%;
   padding: 15px;
+}
+.card * {
+  color: #fff;
+}
+
+.card .media:not(:last-child) {
+  margin-bottom: 1em;
 }
 </style>
