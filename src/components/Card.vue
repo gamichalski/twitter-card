de5999ef954d535a438d13ea5c17379968727248
Gean Farias <gean.farias@outlook.com>
2020-04-18 19:43:38 +0000

    modificação do card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="custom-card">
-    Teste
+    <div class="card">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img class="is-rounded" src="https://pbs.twimg.com/profile_images/1250960895422971904/bAcfaoj0_400x400.jpg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4 is-marginless">Gean Farias</p>
+        <p class="subtitle is-6 is-marginless">
+            24 anos, Joinville - SC
+        </p>
+        <p class="subtitle is-6">
+            <a href="https://twiiter.com/migoseutonto" target="_blank">@migoseutonto</a>
+            ||
+            <a href="mailto:geab@ocubomagi.co" target="_blank">gean@ocubomagi.co</a>
+        </p>
+      </div>
+    </div>
+
+    <div class="content">
+      Desenvolvedor frontend, formando em Ciência da computação e amante das comidas do norte (em especial de Manaus).
+      <div class="tags">
+          <span class="tag is-dark">#scss</span>
+          <span class="tag is-dark">#responsive</span>
+          <span class="tag is-dark">#html5</span>
+          <span class="tag is-dark">#vue</span>
+          <span class="tag is-dark">#js</span>
+          <span class="tag is-dark">#cozinheiro</span>
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
